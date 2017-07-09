e3b77fe917e9f2d12e08746a46500df7165de574
Gabriel Reis Miranda <gabrielmiranda@codeondemand.com.br>
2017-07-09 12:01:00 -0300

    MERGE

---
+++ front-end/dashboard/assets/modal/exposicao.js
@@ -0,0 +1,15 @@
+export class exposicao {
+  id_exposicao;
+  data_inicio;
+  data_termino;
+  categoria;
+  descricao;
+  ativo;
+  titulo;
+  item;
+  remove_exposicao;
+  edita_exposicao;
+  cria_exposicao;
+
+  constructor() {}
+}
