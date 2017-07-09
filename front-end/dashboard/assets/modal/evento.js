c3c636e2a8e5479141f6d157b79c22acbdb3d357
Gabriel Reis Miranda <gabrielmiranda@codeondemand.com.br>
2017-07-09 12:29:09 -0300

    Model (falta definir alguns objetos)

---
+++ front-end/dashboard/assets/modal/evento.js
@@ -0,0 +1,19 @@
+export class evento { 
+  id_evento; 
+  titulo; 
+  local; 
+  responsavel; 
+  foto_url; 
+  atista; 
+  horario_visitacao; 
+  data_inicio; 
+  data_fim; 
+  categoria; 
+  ativo;
+  cria_evento;
+  edita_evento;
+  remove_evento;
+  favorita_evento;
+ 
+  constructor() {} 
+}
---
+++ front-end/dashboard/assets/modal/usuario.js
@@ -0,0 +1,14 @@
+export class usuario { 
+  id_usuario; 
+  nome; 
+  sobrenome; 
+  facebook_id; 
+  google_id; 
+  ativo; 
+  token; 
+  data_token; 
+  tipo; 
+  permissao; 
+ 
+  constructor() {} 
+}
