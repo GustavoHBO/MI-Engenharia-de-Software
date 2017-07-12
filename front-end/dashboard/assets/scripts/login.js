436d0657cda3c4d41e582de7567cb72fc91f31d5
jrnmagalhaes <seuze.rick@gmail.com>
2017-07-11 23:57:39 -0300

    configuração do firebase auth

--- front-end/dashboard/assets/scripts/login.js
+++ front-end/dashboard/assets/scripts/login.js
@@ -1,5 +1,33 @@
 var loginpage = new Vue({
   el: '#login-page',
   data: {
+    email: "",
+    senha: ""
+  },
+  methods:{
+    loginEmail: () => {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.senha).catch(function(error) {
+      // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorMessage);
+      // ...
+      });
+
+      firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+              window.location.href = "index.html";
+          } else {
+              console.log('não logado');
+              // No user is signed in.
+          }
+      });
+    },
+    loginGoogle: () => {
+      console.log('apertou google');
+    },
+    loginFacebook: () => {
+      console.log('apertou facebooks');
+    }
   }
 })
