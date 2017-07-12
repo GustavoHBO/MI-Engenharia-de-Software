a6747f3a64e8b9bb093f4b7e5f209cd5366c192d
MrGustavo <ghboliveira@hotmail.com>
2017-07-11 21:45:53 -0300

    Alteração na palavra usuário no dropdown do menu em todas as páginas, criação do vue nas páginas de gerenciamento de usuário e funcionário e finalização da página de novo item

---
+++ front-end/dashboard/assets/scripts/gerenciar-funcionarios.js
@@ -0,0 +1,13 @@
+var lista_funcionarios = new Vue({
+  el: '#lista_funcionarios',
+  data: {
+    funcionarios: [
+        {
+            nome: 'Carlos Rodrigues'
+        },
+        {
+            nome: 'Maikon Filho'
+        }
+    ]
+  }
+})
---
+++ front-end/dashboard/assets/scripts/gerenciar-usuarios.js
@@ -0,0 +1,13 @@
+var lista_usuarios = new Vue({
+  el: '#lista_usuarios',
+  data: {
+    usuarios: [
+        {
+            nome: 'Carlos Goes'
+        },
+        {
+            nome: 'Titi Queiroz'
+        }
+    ]
+  }
+})
