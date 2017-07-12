0ad45b9c2f4dc58617136ca166fe59b607464c39
jrnmagalhaes <seuze.rick@gmail.com>
2017-07-12 00:24:03 -0300

    login com google funcionando

--- front-end/dashboard/assets/scripts/login.js
+++ front-end/dashboard/assets/scripts/login.js
@@ -24,7 +24,18 @@ var loginpage = new Vue({
       });
     },
     loginGoogle: () => {
-      console.log('apertou google');
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log('logou');
+      window.location.href = "index.html";
+      }).catch(function(error) {
+        console.log(errorMessage = error.message);
+      });
     },
     loginFacebook: () => {
       console.log('apertou facebooks');
