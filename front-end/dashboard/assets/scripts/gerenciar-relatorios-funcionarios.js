a37fb9d15cad6f1df7bd0c29a5520b94ecf0c181
MrGustavo <ghboliveira@hotmail.com>
2017-07-17 14:11:42 -0300

    Finalização das telas de gerenciamento de funcionários e gerenciamento de usuários

---
+++ front-end/dashboard/assets/scripts/gerenciar-relatorios-funcionarios.js
@@ -0,0 +1,17 @@
+var lista_relatorio = new Vue({
+  el: '#lista_relatorio',
+  data: {
+    relatorios: [
+    {
+      nome: 'Carlos Rodrigues',
+      item: 'Bota de Couro',
+      movimentacao: 'Adição'
+    },
+    {
+      nome: 'Fernando Guigs',
+      item: 'Carroça de Madeira',
+      movimentacao: 'Remoção'
+    }
+    ]
+  }
+});
---
+++ front-end/dashboard/assets/scripts/gerenciar-relatorios-itens.js
@@ -0,0 +1,17 @@
+var lista_relatorio = new Vue({
+  el: '#lista_relatorio',
+  data: {
+    relatorios: [
+    {
+      item: 'Bota de Couro',
+      funcionario: 'Carlos Rodrigues',
+      movimentacao: 'Adição'
+    },
+    {
+      item: 'Carroça de Madeira',
+      funcionario: 'Fernando Guigs',
+      movimentacao: 'Remoção'
+    }
+    ]
+  }
+});
