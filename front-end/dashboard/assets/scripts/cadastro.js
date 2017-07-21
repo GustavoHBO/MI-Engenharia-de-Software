9ff46a5f264682f7a9e2a766d545616a1354e947
jrnmagalhaes <seuze.rick@gmail.com>
2017-07-20 23:14:04 -0300

    avisos ao usuário de que ele não conseguiu realizar o cadastro por senha fraca e por motivo do e-mail já estar em uso

--- front-end/dashboard/assets/scripts/cadastro.js
+++ front-end/dashboard/assets/scripts/cadastro.js
@@ -11,9 +11,24 @@ var cadastroPage = new Vue({
     createUser: function (event) {
         event.preventDefault();
         firebase.auth().createUserWithEmailAndPassword(this.email, this.senha).catch(function (error) {
-            console.log(error.code);
-            console.log(this.email)
-            console.log(this.senha)
+            let erro = error.code;
+            if(erro == 'auth/weak-password'){
+                $.gritter.add({
+                    // (string | mandatory) the heading of the notification
+                    title: 'Senha muito fraca!',
+                    // (string | mandatory) the text inside the notification
+                    text: 'A senha deve conter no mínimo 8 caracteres',
+                    class_name: 'gritter-light'
+                });
+            } else if(erro == 'auth/email-already-in-use'){
+                $.gritter.add({
+                    // (string | mandatory) the heading of the notification
+                    title: 'E-mail inválido!',
+                    // (string | mandatory) the text inside the notification
+                    text: 'Este e-mail já se encontra em uso',
+                    class_name: 'gritter-light'
+                });
+            }
         })
     }
   }
