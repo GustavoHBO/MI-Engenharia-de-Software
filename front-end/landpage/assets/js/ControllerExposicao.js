722c854791773e4947a3000e74f7f60fd461415e
MrGustavo <ghboliveira@hotmail.com>
2017-08-17 23:38:59 -0300

    Vue.js na página de exposições

--- front-end/landpage/assets/js/ControllerExposicao.js
+++ front-end/landpage/assets/js/ControllerExposicao.js
@@ -13,6 +13,7 @@ class ControllerExposicao {
                     data_inicio: '25/04/2017',
                     data_fim: '05/05/2017',
                     descricao: 'Exposição de obras sobre a cultura brasileira do século 20',
+                    imagem: "http://placehold.it/400x300",
                     itens: [{id: 1,
                         nome: 'Calça de couro',
                         autor: 'Carlos Manoel',
@@ -28,26 +29,59 @@ class ControllerExposicao {
             }
         });
     }
-
-    item() {
-        var item = new Vue({
-            el: '#item',
+    listarExposicoes() {
+        var listarExposicoes = new Vue({
+            el: '#lista-exposicoes',
             data: {
-                favorito:  false
+                exposicao_principal: {
+                    id: 1,
+                    titulo: 'Exposição de Obras',
+                    categoria: 'Exibição Temporária',
+                    data_inicio: '25/04/2017',
+                    data_fim: '05/05/2017',
+                    descricao: 'Exposição de obras sobre a cultura brasileira do século 20',
+                    imagem: "http://placehold.it/400x300",
+                    itens: [{id: 1,
+                        nome: 'Calça de couro',
+                        autor: 'Carlos Manoel',
+                        image: 'http://placehold.it/400x300',
+                        descricao: 'Calça de couro de cavalo de 1941'
+                    }]
+                },
+                exposicoes: [{
+                    id: 1,
+                    titulo: 'Exposição de Obras',
+                    categoria: 'Exibição Temporária',
+                    data_inicio: '25/04/2017',
+                    data_fim: '05/05/2017',
+                    descricao: 'Exposição de obras sobre a cultura brasileira do século 20',
+                    imagem: "http://placehold.it/400x300",
+                    itens: [{id: 1,
+                        nome: 'Calça de couro',
+                        autor: 'Carlos Manoel',
+                        image: 'http://placehold.it/400x300',
+                        descricao: 'Calça de couro de cavalo de 1941'
+                    }]
+                },{
+                    id: 1,
+                    titulo: 'Exposição de Obras',
+                    categoria: 'Exibição Temporária',
+                    data_inicio: '25/04/2017',
+                    data_fim: '05/05/2017',
+                    descricao: 'Exposição de obras sobre a cultura brasileira do século 20',
+                    imagem: "http://placehold.it/400x300",
+                    itens: [{id: 1,
+                        nome: 'Calça de couro',
+                        autor: 'Carlos Manoel',
+                        image: 'http://placehold.it/400x300',
+                        descricao: 'Calça de couro de cavalo de 1941'
+                    }]
+                }]
             },
-            methods: {
-                cliquei: () => {
-                    if(this.favorito){
-                        $(".star.glyphicon").click(function() {
-                            $(this).toggleClass("glyphicon-star-empty");
-                        });
-                    } else{
-                        $(".star.glyphicon").click(function() {
-                            $(this).toggleClass("glyphicon-star");
-                        });
-                    }
-                    this.favorito = !this.favorito;
-                }
+            created: () => {
+
+                //Depois que pegar a lista de itens, separar a manchete principal, que no caso será o primeiro da lista.
+                
             }
         });
     }
